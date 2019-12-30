3564b0ae0061f50a5666901ef344499ac4155708
cohen3 <eladc5492@gmail.com>
2019-12-30 10:09:43 +0200

    added random walk

--- __main__.py
+++ __main__.py
@@ -9,6 +9,7 @@ from configuration.configuration import getConfig
 from dataset_loader.loader import data_loader
 from graph_generation.full_graph_generation import graph_generation
 from graph_generation.sub_graph_generation_random_selection import random_selection
+from graph_generation.sub_graph_generation_random_walk import random_walk
 from graph_generation.graph_feature_extraction import graph_feature_extraction
 from dataset_generation.xgboost_dataset_generator import xgboost_generator
 from DB.csv_db import CSV_DB
@@ -23,6 +24,7 @@ db = CSV_DB()
 modules_dict['data_loader'] = data_loader
 modules_dict['graph_generation'] = graph_generation
 modules_dict['random_selection'] = random_selection
+modules_dict['random_walk'] = random_walk
 modules_dict['graph_feature_extraction'] = graph_feature_extraction
 modules_dict['xgboost_generator'] = xgboost_generator
 
--- graph_generation/sub_graph_generation_random_walk.py
+++ graph_generation/sub_graph_generation_random_walk.py
@@ -1,4 +1,6 @@
+import itertools
 import os
+import random
 
 from configuration.configuration import getConfig
 from tool_kit.AbstractController import AbstractController
@@ -8,6 +10,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 from networkx.algorithms.clique import *
+from networkx.algorithms.traversal import *
 import random as rnd
 
 
@@ -18,39 +21,22 @@ class random_walk(AbstractController):
         self.db = db
 
     def setUp(self):
+        self.method = getConfig().eval(self.__class__.__name__, "method")
         self.corr_threshold = getConfig().eval(self.__class__.__name__, "corr_threshold")
-        self.vertex_threshold = getConfig().eval(self.__class__.__name__, "vertex_threshold")
-        # self.dataset_table = getConfig().eval(self.__class__.__name__, "dataset_table")
-        self.num_of_subgraphs = getConfig().eval(self.__class__.__name__, "num_of_subgraphs_each")
-        self.clear_existing_subgraphs = getConfig().eval(self.__class__.__name__, "clear_existing_subgraphs")
 
 
 
     def execute(self, window_start):
-        # clear old gpickle graph files
-        if self.clear_existing_subgraphs:
-            print('removing old subgraphs')
-            self.clear_graphs()
-
         # execute random walk
         print('read from full graph:')
         datasets = pd.read_csv('data/dataset_out/target_features.csv')['dataset_name'].tolist()
         print("data sets: {}".format([i for i in datasets]))
         graph_id = 1
-        datasets.remove('data_10000_features_corr_graph')
         for data in datasets:
             print(bcolors.BOLD + bcolors.UNDERLINE + bcolors.OKBLUE + 'Dataset: ' + data
                   + bcolors.ENDC + bcolors.ENDC + bcolors.ENDC)
-            # if self.db.is_csv:
-            #     full_graph = pd.read_csv('data/dataset_out/dataset_feature_correlation.csv')
-            #     full_graph = full_graph.loc[full_graph['dataset_name'] == data + ".csv"].values.tolist()
-            # else:
-            #     full_graph = self.db.execQuery(
-            #         'SELECT * FROM ' + self.dataset_table + ' WHERE dataset_name=\'' + data + '\'')
             # calculate number of max vertexes
             df = pd.read_csv('data/dataset_out/'+data+'.csv')
-            graph_len = len(df.columns) - 1
-            max_vertexes = int(graph_len * self.vertex_threshold)
             df = df.set_index(df.columns)
             print('building full graph...')
             full_graph = nx.from_pandas_adjacency(df)
@@ -59,39 +45,37 @@ class random_walk(AbstractController):
                                if full_graph[edge[0]][edge[1]]['weight'] < self.corr_threshold]
             print('removing edges...')
             full_graph.remove_edges_from(egdes_to_remove)
-            subgraphs_set = []
-            # generate as many sub graphs as indicated in the config file
-            nodes_count = len(full_graph.nodes) - 1
-            g = 0
-            while g < self.num_of_subgraphs:
-                for i in range(int(nodes_count*self.vertex_threshold), nodes_count):
-                    if g >= self.num_of_subgraphs:
-                        break
-                    print('working on subgraph ' + str(graph_id))
-                    sub = rnd.sample(list(full_graph.nodes), i)
-                    sub = sorted(sub)
-                    if sub in subgraphs_set:
-                        continue
-                    subgraphs_set.append(sub)
-                    g1 = full_graph.subgraph(sub)
-                    nx.write_gpickle(g1, 'data/sub_graphs/' + data + '_subgraph' + str(graph_id) + '.gpickle')
-                    graph_id += 1
-                    g += 1
-                    # TODO: perform the random walk
-                    # TODO: dont allow empty graphs
-                    # TODO: should we connect non connected nodes with weight 0 o make the graph connected?
+            if self.method == 'clique' or self.method == 'all':
+                print('finding cliques...')
+                cliques = nx.find_cliques(full_graph)
+                self.__save_subgraphs(cliques, data, 'clique')
+            if self.method == 'tree' or self.method == 'all':
+                print('building trees...')
+                sources = random.choices(list(full_graph.nodes), k=len(list(full_graph.nodes))*0.1)
+                for node in sources:
+                    tree = dfs_tree(full_graph, source=node, depth_limit=int(len(full_graph)/2))
+                    self.__save_subgraphs([tree], data, 'tree')
 
+
+    def __save_subgraphs(self, graph_iter, name, kind):
+        graph_id = 1
+        try:
+            for sub in graph_iter:
+                sub_graph = self.complete_graph_from_list(list(sub))
+                nx.write_gpickle(sub_graph, 'data/sub_graphs/' + name + '_subgraph_' + kind + str(graph_id) + '.gpickle')
+                graph_id += 1
+        except MemoryError as me:
+            pass
+
+    def complete_graph_from_list(self, L, create_using=None):
+        G = nx.empty_graph(len(L), create_using)
+        if len(L) > 1:
+            edges = itertools.combinations(L, 2)
+            G.add_edges_from(edges)
+        return G
 
     def clear_graphs(self):
         filelist = [f for f in os.listdir('data/sub_graphs') if f.endswith(".gpickle")]
         for f in filelist:
             os.remove(os.path.join('data/sub_graphs', f))
 
-    def __connected_components(self, G):
-        pass
-
-    def __find_cliques(self, G):
-        cliques = enumerate_all_cliques(G)
-        for clique in cliques:
-            print(clique)
-        pass
